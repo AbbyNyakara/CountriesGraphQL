6ec902e1f237a531ad4b36085b9f1e850be354b5
Abby Nyakara <abbynyakara@gmail.com>
2023-03-20 09:28:08 +0300

    add country detail page

--- src/hooks/useCountry.js
+++ src/hooks/useCountry.js
@@ -1,32 +1,30 @@
 import { useQuery, gql } from "@apollo/client";
 
 const GET_COUNTRY = gql`
-  query GetCountry($code: ID!) {
+  query GetCountry($code: ID!){
     country(code: $code){
       name,
       capital,
+      currency,
       continent{
         name
       },
-      capital,
-      currency,
       languages{
-        name
+        name,
+        native
       }
     }
   }
 `
 
+// Create the function 
+
 export const useCountry = (code) => {
-  const { error, loading, data } = useQuery(GET_COUNTRY, {
-    variables: {
-      code
-    }
-  });
+  const { error, loading, data } = useQuery(GET_COUNTRY, {variables: {
+    code
+  }});
 
   return {
-    data,
-    error,
-    loading
+    error, loading, data
   }
 }
--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Error from './Error';
 import { useQuery, gql } from '@apollo/client';
 import loadingImg from '../assets/loading.gif';
@@ -40,7 +41,7 @@ const Countries = () => {
   if (error) return (
   <div className='min-h-screen text-4xl flex flex-col justify-center items-center'>
       <img src={errorImg} alt="error page" className='min-w-[30rem] mb-4' />
-      <h2>We've run into a yuuuuuuuuge problem!</h2>
+      <h2>Something Went Wrong!</h2>
       <p className='text-xl'>{error.message}</p> 
     </div>
   ) 
@@ -50,18 +51,18 @@ const Countries = () => {
       {
         data.countries.map(country => {
           return(
-            <div key={country.code} className="shadow-md bg-white m-4 p-4 rounded">
+            <Link to={`/${country.code}`} key={country.code} className="shadow-md bg-white m-4 p-4 rounded">
               <p className='font-semibold'>Country Name: <span className='font-normal'>{country.name}</span></p>
               <p className='font-semibold'>Capital City: <span className='font-normal'>{country.capital}</span></p>
               <p className='font-semibold'>Continent: <span className='font-normal'>{country.continent.name}</span></p>
               <p className='font-semibold'>Currency: <span className='font-normal'>{country.currency}</span></p>
-              <p>{country.code}</p>
+              {/* <p>{country.code}</p> */}
               {
                 country.languages.map((language, idx) => {
                   <small key={idx} className='font-semibold'><span>{language}</span></small>
                 })
               }
-            </div>
+            </Link>
           )
         })
       }
