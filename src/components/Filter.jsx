05d64712e81c7dfbbe74b0b336079dbd7a5cf10a
Abby Nyakara <abbynyakara@gmail.com>
2023-03-22 14:04:15 +0300

    add query

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -17,25 +17,17 @@ const FILTER_BY_CONTINENTS = gql`
 
 // Filter component
 const Filter = () => {
-  const [continent, setContinent] = useState("");
+  const [continent, setContinent] = useState("all");
 
   const [getCountries, {error, loading, data}] = useLazyQuery(FILTER_BY_CONTINENTS, {variables: {
     continent
-  }})
+  }});
 
   return (
     <div>
       <form action="" method="get">
-        <select name="countries" className='p-2 shadow-md rounded' value={continent} onChange={(e) => setContinent(e.target.value)}>
-          <option value="all">Filter by continent</option>
-          {
-            continentData?.map((continent, index) => {
-              return <option value={continent} key={index}>{continent}</option>
-            })
-          }
-        </select>
+        
       </form>
-      
     </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLazyQuery, gql } from '@apollo/client'
+
+const FILTER_BY_CONTINENTS = gql`
+  query FilterByContinents($continent: String!) {
+    countries(filter: {
+      continent: {eq: $continent}
+    }) {
+      name,
+      capital,
+      currency
+    }
+  }
+`
+
 
 const Search = () => {
   return (
